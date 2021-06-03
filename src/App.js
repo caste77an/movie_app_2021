2e219ce73aa152208d173f2da7b8f5c4fd7e87ef
jsh <jsh@jshui-iMac.local>
2021-06-03 13:32:50 +0900

    movie_app_2021 with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumb2.gettyimageskorea.com%2Fimage_preview%2F700%2F201308%2FCIF%2FCIF13000152.jpg&type=sc960_832",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "kimbap",
-    image: "https://search.pstatic.net/common/?src=https%3A%2F%2Fmodo-phinf.pstatic.net%2F20210304_193%2F1614841996063Trlbf_JPEG%2Fmosaw2FrAf.jpeg&type=sc960_832",
-    rating: 3.5
-  }
-]
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      {foodILike.map(dish =>(
-        <Food name={dish.name} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Detail} />
+  </HashRouter>
 }
 
 export default App;
